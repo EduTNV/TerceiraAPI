1cf42519844cd59918aa330da78c5f2e888d7876
Eduardo Travassos <eduardotrvassos@gmail.com>
2025-11-11 16:09:04 -0300

    alterações

---
+++ src/main/java/com/example/Alexandre2/MinhasegundaAPI/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.Alexandre2.MinhasegundaAPI.config; // Verifique se o nome do pacote base está correto
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite CORS para todos os endpoints (/**)
+                .allowedOrigins("http://localhost:8080") // Permite solicitações vindas do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
